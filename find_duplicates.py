65a5bbe410161715044d71e7f5f47226b3be069c
Bruno Krinski <brunoak94@gmail.com>
2020-08-24 17:25:19 -0300

    find duplicates

--- find_duplicates.py
+++ find_duplicates.py
@@ -8,23 +8,35 @@ import multiprocessing
 from PIL import Image
 from tqdm import tqdm
 from joblib import Parallel, delayed
-from iteration_utilities import unique_everseen, duplicates, all_equal
 
 def hash_image(image_path):
     image = Image.open(image_path)
-    hashed_image = str(imagehash.average_hash(image))
-
     image_name = image_path.split('/')[-1]
-    width, height = image.size
+    hashes = []
+    for angle in [ 0, 90, 180, 270 ]:
+        if angle > 0:
+            turned_img = image.rotate(angle, expand=True)
+        else:
+            turned_img = image
+        hashes.append(str(imagehash.phash(turned_img)))
+    hashes = ''.join(sorted(hashes))
+    #width, height = image.size
 
     image_infos = {
         'name': image_name,
-        'width': width,
-        'height': height,
-        'hash': hashed_image
+        'hash': hashes
     }
     return image_infos
 
+def find_duplicates(image, image_list):
+    duplicates = []
+    duplicates.append(image)
+    for item in image_list:
+        if image['name'] != item['name'] and image['hash'] == item['hash']:
+            duplicates.append(item)
+    if len(duplicates) > 1:
+        return duplicates
+    return None
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -47,32 +59,22 @@ if __name__ == '__main__':
 
     hashed_images = Parallel(n_jobs=num_cores)(
                 delayed(hash_image)(image_path) for image_path in image_paths)
+    duplicates = Parallel(n_jobs=num_cores)(
+                delayed(find_duplicates)(item, hashed_images) for item in hashed_images)
 
-    images = list(duplicates(hashed_images, get_hash ))
-    print(images)
-    print(len(images))
-    #print(hashed_images - uniques)
-    
-    '''
-    cont = 1
-    for image_path in images:
-        image_zero = Image.open(image_path)
-        hashes0 = imagehash.average_hash(image_zero)
-        print(image_path)
-        #print(hashes0)
-        for idx in range(cont, num_images):
-            
-            image = Image.open(images[idx])
-            print(images[idx])
-            hashes1 = imagehash.average_hash(image)
-            print(hashes0 - hashes1)
-            if hashes0 == hashes1: 
-                print("--------------------------------")
-                print(images[idx])
-                print("--------------------------------")
-        cont += 1
-        print("===============================================================")
-    ''' 
-        
+    new_folder = 'duplicates'
+    os.makedirs(new_folder, exist_ok=True)
+    names_list = []
+    cont = 0
+    for item in duplicates:
+        if item is not None:
+            for elem in item:
+                if elem['name'] not in names_list:
+                    names_list.append(elem['name'])
+                    old_path = folder_path + '/' + elem['name']
+                    new_path = new_folder + '/' + str(cont) + '_' + elem['name']
+                    os.system('mv ' + old_path + ' ' + new_path)
+                    cont += 1
+
 
         
